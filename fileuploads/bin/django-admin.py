90f20d7b634fa8da4789ba08a3dc90ac55180dd3
tuxfux-hlp <tuxfux.hlp@gmail.com>
2016-07-06 22:05:48 +0530

    filesuploads

---
+++ fileuploads/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/key2gyaan/Documents/git_repositories/tuxfux-hlp/DjangoProjects/fileuploads/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
