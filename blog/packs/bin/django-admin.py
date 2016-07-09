f4d25f6513dad43d05ca7b0cf1f460076895dfd4
tuxfux-hlp <tuxfux.hlp@gmail.com>
2016-07-09 16:06:02 +0530

    1st Project - My blog

---
+++ blog/packs/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/key2gyaan/Documents/git_repositories/tuxfux-hlp/DjangoProjects/blog/packs/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
